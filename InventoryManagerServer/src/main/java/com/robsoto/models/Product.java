0dc059a26018d3465f01c6962c0937a9768dd01d
Roberto Soto <rsoto1121@gmail.com>
2023-08-29 22:54:35 -0400

    model getters/setters

--- InventoryManagerServer/src/main/java/com/robsoto/models/Product.java
+++ InventoryManagerServer/src/main/java/com/robsoto/models/Product.java
@@ -1,5 +1,7 @@
 package com.robsoto.models;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,5 +27,57 @@ public class Product {
 	@ManyToOne
 	@JoinColumn(name = "warehouse_id")
 	private Warehouse warehouse;
+	
+	public Product() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Warehouse getWarehouse() {
+		return warehouse;
+	}
+
+	public void setWarehouse(Warehouse warehouse) {
+		this.warehouse = warehouse;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name, warehouse);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(description, other.description) && id == other.id && Objects.equals(name, other.name)
+				&& Objects.equals(warehouse, other.warehouse);
+	}
 
 }
--- InventoryManagerServer/src/main/java/com/robsoto/models/Warehouse.java
+++ InventoryManagerServer/src/main/java/com/robsoto/models/Warehouse.java
@@ -1,5 +1,6 @@
 package com.robsoto.models;
 
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -31,5 +32,57 @@ public class Warehouse {
 	@OneToMany(mappedBy = "warehouse", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Set<Product> products;
+	
+	public Warehouse() {}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, maxCapacity, name, products);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Warehouse other = (Warehouse) obj;
+		return id == other.id && maxCapacity == other.maxCapacity && Objects.equals(name, other.name)
+				&& Objects.equals(products, other.products);
+	}
 
 }
