bcbfe5b0ffee92101281a2e12659220ac8060ff8
Roberto Soto <rsoto1121@gmail.com>
2023-08-31 16:36:51 -0400

    product controller findAll, findById

--- InventoryManagerServer/src/main/java/com/robsoto/controllers/ProductController.java
+++ InventoryManagerServer/src/main/java/com/robsoto/controllers/ProductController.java
@@ -1,9 +1,16 @@
 package com.robsoto.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.robsoto.models.Product;
+import com.robsoto.models.Warehouse;
 import com.robsoto.services.ProductService;
 
 @RestController
@@ -14,6 +21,26 @@ public class ProductController {
 	
 	public ProductController(ProductService service) {
 		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> products = service.findAll();
+		if (products == null || products.size() == 0) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+		return new ResponseEntity<>(products, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findById(@PathVariable int id) {
+		Product product = service.findById(id);
+		if (product == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>(product, HttpStatus.OK);
 	}
 	
 }
